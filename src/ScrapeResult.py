c4b9ad14b4ba08bfe1eb2722be120d040281e5b3
Galnerd <e0543848@u.nus.edu>
2023-11-06 18:38:37 +0800

    scraper extracts ip address, response time and links found in url

--- src/ScrapeResult.py
+++ src/ScrapeResult.py
@@ -2,20 +2,24 @@ import typing
 
 class ScrapeResult:
 
-    def __init__(self):
+    def __init__(self, ip_address, response_time, links):
         print('ScrapeResult initialised')
 
-        # Response time: time from sending the request to receiving the reply
-        # Data type: int/float?
-        self.response_time = None
-
         # IP address of the server
-        # Data type: str (could use ipaddress python module but lazy)
-        self.server_ip = None
+        # Data type: str
+        self.server_ip = ip_address
+
+        # Response time: time from sending the request to receiving the reply
+        # Data type: float
+        self.response_time = response_time
 
         # Region of the server
-        # Data type: str
+        # Data type: str TODO
         self.server_region = None
+
+        # Links found in the html
+        # Data type: list of str
+        self.links = links
 
         # Open ended requirement, still TBC (read assignment doc page 1)
         # Considering a dictionary mapping some search terms to their frequency of occurance
--- src/Scraper.py
+++ src/Scraper.py
@@ -1,18 +1,87 @@
 import typing
 from typing import List
 
+import socket # to resolve ip address given hostname
+import urllib.parse # to parse hostname given url
+import requests # to send http requests
+import time # to get response time
+from bs4 import BeautifulSoup # to parse the webpage
+
 from ScrapeResult import ScrapeResult
 
 class Scraper:
 
     def __init__(self, url: str, search_terms: List[str]):
         print('Scraper initialised')
-        pass
+        self.url = url
+        self.search_terms = search_terms
 
     def scrape(self) -> ScrapeResult:
         print('Scraper scraping')
+
+        ip_address = Scraper.get_ip_address(self.url)
+        if not ip_address:
+            print("Failed to resolve ip address")
+            return None
+
+        tmp = Scraper.send_html_request(self.url)
+        if not tmp:
+            print("Failed to send html request")
+            return None
+        
+        response = tmp[0]
+        response_time = tmp[1]
+
+        print(f"IP Address: {ip_address}")
+        print(f"Response time: {response_time}")
+
+        links = Scraper.extract_links_from_response(response)
+        links = [urllib.parse.urljoin(self.url, link) for link in links]
+
+        return ScrapeResult(ip_address, response_time, links)
         # send the necessary http requests,
         # parse http response and search for search_terms and their frequency of occurance,
         # get geolocation data,
         # put all the data into a ScrapeResult and return it
-        pass
+    
+    def send_html_request(url):
+        try:
+            # Send an HTTP GET request to the specified URL
+            start_time = time.time()
+            response = requests.get(url, timeout=5)
+            end_time = time.time()
+            if response.status_code == 200:
+                response_time = end_time - start_time
+                return (response, response_time)
+            else:
+                print(f"HTTP GET request failed with status code: {response.status_code}")
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while making the request: {e}")
+            return None
+
+    def extract_links_from_response(response):
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find all <a> tags in the parsed HTML
+        links = []
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href and href != "#":
+                links.append(href)
+
+        return links
+
+    def get_ip_address(url):
+        try:
+            parsed_url = urllib.parse.urlparse(url)
+            host = parsed_url.netloc
+            ip_address = socket.gethostbyname(host)
+            return ip_address
+        except (socket.gaierror, ValueError):
+            return None
+
+scraper = Scraper("http://wikipedia.com", [])
+links = scraper.scrape().links
+for link in links:
+    print(link)
