1dce2b813ffc128514cbd1fafa91af8d20158ce9
Ruth <ruthpoh99@gmail.com>
2023-11-08 17:32:58 +0800

    Edit main file delete unused parameter starting url

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from Crawler import Crawler
 
 if __name__ == '__main__':
-    crawler = Crawler('test_url',
-                      ['test_search_term_1', 'test_search_term_2'],
+    crawler = Crawler(['test_search_term_1', 'test_search_term_2'],
                       3,
                       10,
                       3)
